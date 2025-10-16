10717a9510c859e321724862f72650f6b004a574
wagnermazevedo <136827872+wagnermazevedo@users.noreply.github.com>
2025-10-16 08:12:21 -0300

    Update run-multicloudassessment.sh

--- run-multicloudassessment.sh
+++ run-multicloudassessment.sh
@@ -136,6 +136,7 @@ aws)
 
       log "INFO" "▶️ Executando Prowler Azure..."
       prowler azure \
+        --sp-env-auth \
         -M csv html json-asff \
         --output-filename "multicloudassessment-azure-${ACCOUNT_ID}.json" \
         --output-directory "$OUTPUT_DIR" \
